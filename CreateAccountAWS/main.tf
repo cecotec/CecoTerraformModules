568d4eb034101f457117d77499931d9b43f4b72c
ceocITec <68845515+ceocITec@users.noreply.github.com>
2021-01-18 15:48:38 +0100

    Added email and fixed for_each

--- CreateAccountAWS/main.tf
+++ CreateAccountAWS/main.tf
@@ -1,9 +1,11 @@
+locals {
+    envs = toset(["dev", "qa", "pre", "prod"])
+}
+
 resource "aws_organizations_account" "account"{
    for_each = local.envs
    name = "${var.project_name}-${each.value}"
    parent_id = var.parent_id
+   email = "awsprojec+${var.project_name}-${each.value}@cecotec.es"
 }
 
-locals {
-    envs = ["dev", "qa", "pre", "prod"]
-}
