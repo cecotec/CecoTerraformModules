df262d4943a936f8fce87b13d297045735255d88
Tiko <vjgarcera@gmail.com>
2021-01-19 09:30:44 +0100

    Fixed typo in email

--- CreateAccountAWS/main.tf
+++ CreateAccountAWS/main.tf
@@ -6,6 +6,6 @@ resource "aws_organizations_account" "account"{
    for_each = local.envs
    name = "${var.project_name}-${each.value}"
    parent_id = var.parent_id
-   email = "awsprojec+${var.project_name}-${each.value}@cecotec.es"
+   email = "awsproject+${var.project_name}-${each.value}@cecotec.es"
 }
 
